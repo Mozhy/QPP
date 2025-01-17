29aeb05581703d4b0e081590df0e8dac88cf08e1
Mozhy <35628274+Mozhy@users.noreply.github.com>
2025-01-17 05:40:47 -0800

    Create NQC.py

---
+++ NQC.py
@@ -0,0 +1,32 @@
+# Calculate Clustering NQC
+results = pd.DataFrame(columns=['qid','score'])
+for i,row in dev_queries.iterrows():
+    qid = row['id']
+    retrieved_list = all_result.loc[all_result['qid'] == qid]
+    
+    res = next((item for item in clustered_data if item["qid"] == qid), None)
+    lables = res["lables"]
+    medoids_indices = res["medoids_indices"]
+
+    nqc_score = 0
+    score_D = ScoreCorpus[f"{qid}"]
+   
+    df = pd.DataFrame({'qid': retrieved_list["qid"], 'docid': retrieved_list["docid"], 'score': retrieved_list["score"],'cluster': lables})
+
+    for g, data in df.groupby('cluster'):
+        medoid_doc = retrieved_list['score'].iloc[medoids_indices[g]]
+        best_docs = data.head(1)
+        for j,row1 in best_docs.iterrows():
+            best_doc_score = row1["score"]
+            nqc_score += pow((best_doc_score - medoid_doc),2)
+            
+    k = len(medoids_indices) * len(best_docs)
+    nqc_score = nqc_score / k
+
+    nqc_score = math.sqrt(nqc_score)
+    nqc_score = nqc_score / score_D
+        
+    new_data = pd.DataFrame([[ qid , nqc_score ]],columns= results.columns)
+    results = pd.concat([results, new_data],ignore_index=True)
+
+results.to_csv('Dev/K-means/nqc_scores/51.txt', header=None, index=None, sep='\t', mode='w')
